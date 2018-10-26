ea70f317c0348bf3efaafe7da5cbbf913fc14e59
Aziz Uysal <azizuysal@gmail.com>
2018-10-26 11:31:32 -0700

    Added test targets to both projects.

--- NetKitExample/NetKitExampleTests/NetKitExampleTests.swift
+++ NetKitExample/NetKitExampleTests/NetKitExampleTests.swift
@@ -1,6 +1,6 @@
 //
-//  NetKitTests.swift
-//  NetKitTests
+//  NetKitExampleTests.swift
+//  NetKitExampleTests
 //
 //  Created by Aziz Uysal on 10/26/18.
 //  Copyright © 2018 Aziz Uysal. All rights reserved.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class NetKitTests: XCTestCase {
+class NetKitExampleTests: XCTestCase {
   
   override func setUp() {
     // Put setup code here. This method is called before the invocation of each test method in the class.
